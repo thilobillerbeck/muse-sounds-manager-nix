43b83c6947dfe2a39479e465e1331eb7f690d399
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-02-13 23:47:58 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "A simple Go package";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      version = "1.1.0.587";
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          muse-sounds-manager-bin = pkgs.stdenv.mkDerivation {
+            name = "muse-sounds-manager-bin";
+            nativeBuildInputs = with pkgs; [ rpmextract ];
+
+            src = pkgs.fetchurl {
+              url = "https://web.archive.org/web/20240213215205/https://muse-cdn.com/Muse_Sounds_Manager_Beta.rpm";
+              sha256 = "sha256-GQLJt3j8LLRTQ4E59vz75IAp9ePetap7RFvat0umifs=";
+            };
+
+            unpackPhase = ''
+              rpmextract $src
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              mkdir -p $out/share
+              cp -r opt/muse-sounds-manager/* $out
+              cp -r usr/share/* $out/share
+            '';
+          };
+        in
+        {
+          muse-sounds-manager =  pkgs.buildFHSEnv {
+            name = "muse-sounds-manager";
+
+            targetPkgs = pkgs: with pkgs; [
+              xorg.libX11
+              xorg.libSM
+              xorg.libICE
+              fontconfig
+              zlib
+              openssl
+              libz
+              icu
+            ];
+
+            extraInstallCommands = ''
+              mkdir -p $out/share
+              cp -r ${muse-sounds-manager-bin}/share/* $out/share
+            '';
+
+            runScript = "${muse-sounds-manager-bin}/Muse.Client.Linux";
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.muse-sounds-manager);
+    };
+}
